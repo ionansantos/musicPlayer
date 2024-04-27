94016ecf8aa0ab31f0181ca697d7a73cb6207caf
ionan santos <ionan564@gmail.com>
2024-04-27 11:22:55 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import AudioList from './src/components/AudioPlayer/AudioList';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  return(
+    <View styles={styles.container}>
+        <StatusBar style='auto'></StatusBar>
+        <View style={{height:44}}></View>
+        <AudioList></AudioList>
     </View>
-  );
+  )
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: '#fff'
+  }
+})
